0322f5e9e633b1d4bb897f8a5f7dc9a6ca446498
maxime blanc <maximeblanc.dev@gmail.com>
2017-12-02 08:18:50 +0100

    wip elixir

--- lib/day1.ex
+++ lib/day1.ex
@@ -2,12 +2,15 @@ defmodule Aoc.Year2017.Day1 do
     def part1 puzzle do
         digit = ""
         nextDigit = ""
+        sum = 0
 
         puzzle 
-        |> String.to_charlist 
-        |> Enum.reduce(fn x -> 
-            IO.puts "voy a la plya issou bido mare"
+        |> String.graphemes
+        |> Enum.each(fn x -> 
+            sum = x + sum
         end)
+
+        sum
     end
     
 end

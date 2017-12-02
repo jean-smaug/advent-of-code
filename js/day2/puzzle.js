8a412a5cb7f4b98d1bccd20dff62279eeaafade2
maxime blanc <maximeblanc.dev@gmail.com>
2017-12-02 14:20:43 +0100

    day two complete

--- js/day2/puzzle.js
+++ js/day2/puzzle.js
@@ -1,5 +1,9 @@
-const { partOne } = require("./day2");
+const { partOne, partTwo } = require("./day2");
 
 partOne("input.txt", response => {
   console.log("Part 1", response);
+});
+
+partTwo("input.txt", response => {
+  console.log("Part 2", response);
 });

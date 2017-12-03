1c08ef56b0071a41aaedb33e6902900cc6299c77
maxime blanc <maximeblanc.dev@gmail.com>
2017-12-03 15:38:30 +0100

    init the fullfilment of map

--- js/day3/day3.js
+++ js/day3/day3.js
@@ -1,4 +1,34 @@
 exports.partOne = puzzle => {
+  let map = []
+  const directions = {
+    right: {
+      direction: "right",
+      nextDireaction: "up"
+    },
+    up: {
+      direction: "up",
+      nextDireaction: "left"
+    },
+    left: {
+      direction: "left",
+      nextDireaction: "down"
+    },
+    down: {
+      direction: "down",
+      nextDireaction: "right"
+    },
+  }
+  let direction = directions.right
+  for (let i = 1; i <= puzzle; i++) {
+    map.push({
+      direction: direction.direction,
+      value: i
+    })
+    if (true) {
+      direction = directions[direction.nextDireaction]
+    }
+  }
+  console.log(map)
   return 0;
 };
 

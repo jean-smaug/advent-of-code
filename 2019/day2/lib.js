cccd69a972e5632b4a0041478050ec0c67ad833c
jean-smaug <maximeblanc.dev@gmail.com>
2019-12-13 15:07:31 +0100

    wip on day2

--- 2019/day2/lib.js
+++ 2019/day2/lib.js
@@ -1,3 +1,27 @@
-module.exports = (mass) => {
-    return Math.floor(mass / 3) - 2
+const first = (line) => {
+    let opCodes = line.split(",").map(Number)
+
+    for (const stringIndex in opCodes) {
+        const index = Number(stringIndex)
+        const opCode = opCodes[index]
+        
+        const firstNumberIndex = opCodes[index + 1]
+        const secondNumberIndex = opCodes[index + 2]
+        const thirdNumberIndex = opCodes[index + 3]
+        
+        if(opCode === 1) {
+            opCodes[thirdNumberIndex] = opCodes[firstNumberIndex] + opCodes[secondNumberIndex]
+        }
+
+        if(opCode === 2) {
+            opCodes[thirdNumberIndex] = opCodes[firstNumberIndex] * opCodes[secondNumberIndex]
+        }
+    }
+
+    return opCodes.join()
 }
+
+const second = (mass) => {}
+
+exports.first = first
+exports.second = second
--- 2019/day2/lib.spec.js
+++ 2019/day2/lib.spec.js
@@ -1,8 +1,14 @@
-const solution = require("./lib.js")
+const { first, second } = require("./lib.js")
 
-it("should resolve puzzle", () => {
-    expect(solution(12)).toBe(2)
-    expect(solution(14)).toBe(2)
-    expect(solution(1969)).toBe(654)
-    expect(solution(100756)).toBe(33583)
+it.only("should resolve part one", () => {
+    expect(first("1,0,0,0,99")).toBe("2,0,0,0,99")
+    expect(first("2,3,0,3,99")).toBe("2,3,0,6,99")
+    expect(first("2,4,4,5,99,0")).toBe("2,4,4,5,99,9801")
+    expect(first("1,1,1,4,99,5,6,0,99")).toBe("30,1,1,4,2,5,6,0,99")
+});
+
+it("should resolve part two", () => {
+    expect(second(14)).toBe(2)
+    expect(second(1969)).toBe(966)
+    expect(second(100756)).toBe(50346)
 });
